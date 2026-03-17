db23104ce8870919794a90b9b0acc30021e16bf4
Guilherme Nogueira <guilherme10.nogueira@gmial.com>
2026-03-17 16:46:14 -0300

    implementação inicial

---
+++ WorkerPoke/Models/PokeApiResponse.cs
@@ -0,0 +1,4 @@
+﻿public class PokeApiResponse
+{
+    public List<PokemonResult> results { get; set; }
+}
---
+++ WorkerPoke/Models/Pokemon.cs
@@ -0,0 +1,6 @@
+﻿public class
+{
+    public int pokemonId { get; set; }
+    public string Nome { get; set; }
+    public string Url { get; set; }
+}
